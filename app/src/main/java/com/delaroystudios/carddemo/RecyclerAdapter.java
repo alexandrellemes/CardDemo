f0effd4d3fd0dae8fb18364e7968ddc0088a6351
Alexandre LLemes <alexandre.llemes@gmail.com>
2019-05-01 22:26:27 -0300

    Alinhamento dos elementos e correção do 5o elemento.

--- app/src/main/java/com/delaroystudios/carddemo/RecyclerAdapter.java
+++ app/src/main/java/com/delaroystudios/carddemo/RecyclerAdapter.java
@@ -14,29 +14,38 @@ import android.widget.TextView;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
-    private String[] titles = {"Chapter One",
+    private String[] titles = {
+            "Chapter One",
             "Chapter Two",
             "Chapter Three",
             "Chapter Four",
             "Chapter Five",
             "Chapter Six",
             "Chapter Seven",
-            "Chapter Eight"};
+            "Chapter Eight"
+    };
 
-    private String[] details = {"Item one details",
-            "Item two details", "Item three details",
-            "Item four details", "Item file details",
-            "Item six details", "Item seven details",
-            "Item eight details"};
+    private String[] details = {
+            "Item one details",
+            "Item two details",
+            "Item three details",
+            "Item four details",
+            "Item five details",
+            "Item six details",
+            "Item seven details",
+            "Item eight details"
+    };
 
-    private int[] images = { R.drawable.android_image_1,
+    private int[] images = {
+            R.drawable.android_image_1,
             R.drawable.android_image_2,
             R.drawable.android_image_3,
             R.drawable.android_image_4,
             R.drawable.android_image_5,
             R.drawable.android_image_6,
             R.drawable.android_image_7,
-            R.drawable.android_image_8 };
+            R.drawable.android_image_8
+    };
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
